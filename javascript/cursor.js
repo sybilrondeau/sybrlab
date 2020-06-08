b64742e6d5a4867403ae54dab8e70cec35bedf50
sybil rondeau <sybilrondeau@macbook-pro-de-sybil-2.home>
2020-06-08 09:52:02 +0200

    animation cursor

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -4,3 +4,14 @@ window.addEventListener("mousemove", e => {
 	mouseCursor.style.top = e.pageY + "px";
 	mouseCursor.style.left = e.pageX + "px";
 });
+
+const links = document.querySelectorAll("a");
+
+links.forEach(link => {
+	link.addEventListener("mouseleave", () => {
+		mouseCursor.classList.remove("link-grow");
+	})
+	link.addEventListener("mouseover", () => {
+		mouseCursor.classList.add("link-grow");
+	})
+});
