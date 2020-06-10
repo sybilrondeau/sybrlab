92812088836b990ed428f6fb2e539c5fdd10c7e3
sybil rondeau <sybilrondeau@macbook-pro-de-sybil-2.home>
2020-06-10 20:28:42 +0200

    mixins media queries

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -2,26 +2,13 @@ const mouseCursor = document.querySelector(".cursor");
 const titles = document.querySelectorAll(".title h2 span");
 const links = document.querySelectorAll("a");
 const imageSybil = document.querySelector(".contact__content");
-// const firstTitle = document.querySelector(".header__title");
-// const position = firstTitle.getBoundingClientRect();
 
 // custom cursor and clipath on titles
 
 window.addEventListener("mousemove", e => {
-	mouseCursor.style.top = (e.pageY-40) + "px";
-	mouseCursor.style.left = (e.pageX-40) + "px";
-	// firstTitle.style.setProperty('--x', (e.pageX-position.x) + "px");
-	// firstTitle.style.setProperty('--y', (e.pageY-position.y) + "px");
+	mouseCursor.style.top = (e.pageY) + "px";
+	mouseCursor.style.left = (e.pageX) + "px";
 });
-
-
-// console.log(position);
-// document.body.onmousemove = function(e) {
-// 	mouseCursor.style.left = (e.clientX) +'px';
-//   	mouseCursor.style.top = (e.clientY) +'px';
-// 	firstTitle.style.setProperty('--x', (e.clientX-position.x) + "px");
-// 	firstTitle.style.setProperty('--y', (e.clientY-position.y) + "px");
-// };
 
 
 // effect on links
