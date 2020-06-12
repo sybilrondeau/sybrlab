fb39ba54e0c225a65e08e7801f94382475e030cc
sybil rondeau <sybilrondeau@macbook-pro-de-sybil-2.home>
2020-06-12 21:54:08 +0200

    media queries portfolio et contact en cours

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -3,30 +3,47 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const animTL = gsap.timeline({
-	scrollTrigger: {
-		trigger: ".portfolio",
-		start: "top top",
-		end: "+=100%",
-		scrub: 1,
-		pin: true,
-		pinSpacing: false,
-		snap: {snapTo: "labels", duration: {min: 0.2, max: 3}, delay: 0.5, ease: "power2.inOut"},
-		toggleClass: {targets:".nav-right li .nav-quoi", className: "active"},
+const mediaQueryList = window.matchMedia("(min-width: 50em)");
+
+function handlePortfolio(mql) {
+	if (mql.matches) {
+
+		const animTL = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".portfolio",
+				start: "top top",
+				end: "+=100%",
+				scrub: 1,
+				pin: true,
+				pinSpacing: false,
+				snap: {snapTo: "labels", duration: {min: 0.2, max: 3}, delay: 0.5, ease: "power2.inOut"},
+				toggleClass: {targets:".nav-right li .nav-quoi", className: "active"},
+			}
+		})
+
+		.add("pic1")
+		.from(".slide--02", {yPercent: 100}, "pic1")
+		.to(".slideText--01", {opacity: 0}, "pic1")
+		.from(".slideText--02", {yPercent: -30, opacity: 0}, "pic1")
+
+		.add("pic2")
+		.from(".slide--03", {yPercent: 100}, "pic2")
+		.to(".slideText--02", {opacity: 0}, "pic2")
+		.from(".slideText--03", {yPercent: -30, opacity: 0}, "pic2")
+
+		// .add("pic3")
+		// .to(".slideText--03", {opacity: 0}, "pic3")
+
+		.add("end")
+	} else {
+		ScrollTrigger.create({
+			trigger: ".portfolio",
+			start: "top bottom",
+			end: "bottom top", 
+			toggleClass: {targets:".nav-right li .nav-quoi", className: "active"}
+		});
 	}
-})
+};
 
-.add("pic1")
-.from(".portfolio__picture--02", {yPercent: 100}, "pic1")
-.to(".slideText--01", {opacity: 0}, "pic1")
-.from(".slideText--02", {yPercent: -30, opacity: 0}, "pic1")
-
-.add("pic2")
-.from(".portfolio__picture--03", {yPercent: 100}, "pic2")
-.to(".slideText--02", {opacity: 0}, "pic2")
-.from(".slideText--03", {yPercent: -30, opacity: 0}, "pic2")
-
-// .add("pic3")
-// .to(".slideText--03", {opacity: 0}, "pic3")
-
-.add("end")
+handlePortfolio(mediaQueryList);
+mediaQueryList.addListener(handlePortfolio);
