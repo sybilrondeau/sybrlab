79639f21bd3e56214587db307801c5a34c98251c
sybil rondeau <sybilrondeau@macbook-pro-de-sybil-2.home>
2020-06-24 14:44:58 +0200

    clean files

--- javascript/drawsvg.js
+++ javascript/drawsvg.js
@@ -3,19 +3,6 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin);
 
-// const svgAnim = gsap.timeline();
-
-// svgAnim.from(".draw", {drawSVG: "20% 80%", duration: 4})
-// 		.to(".draw", {autoAlpha: 1, x: -100});
-
-// ScrollTrigger.create({
-// 	animation: svgAnim,
-// 	trigger: ".draw",
-// 	start: "top 80%",
-// 	toggleActions: "play none none reset",
-// });
-
-
 gsap.from(".draw", {
 	scrollTrigger : {
 		trigger: ".draw",
