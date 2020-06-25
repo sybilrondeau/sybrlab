7304709f8b97156fc6aeaf9e7e52830791fa5560
sybil rondeau <sybilrondeau@macbook-pro-de-sybil-2.home>
2020-06-25 17:33:46 +0200

    fix cta plane bug

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -73,16 +73,21 @@ function handlePortfolio(mql) {
 handlePortfolio(mediaQueryList);
 mediaQueryList.addEventListener('change', handlePortfolio);
 
+// const slideTexts = gsap.utils.toArray(".slideText");
 
-// const portfolioDiv = document.querySelector(".portfolio");
+// // since the tweens alter the inline styles of these elements, we record their state here so that when ScrollTrigger reverts internally (like when switching breakpoints), they get fully reverted with no extra inline styles from tweens. 
+// ScrollTrigger.record(".slide--02, .slideText--01, .slideText--02, .slideText--03, .slideText");
 
-// const myObserver = new ResizeObserver(entries => {
-// 	entries.forEach(entry => {
-// 		if (entry.contentRect.width > 900) {
-// 			console.log(entry);
-// 		const animTL = gsap.timeline({
+// ScrollTrigger.matchMedia({
+
+// 	// desktop
+// 	"(min-width: 900px)": function() {
+// 		// setup animations and ScrollTriggers for screens over 800px wide (desktop) here...
+// 		// ScrollTriggers will be reverted/killed when the media query doesn't match anymore.
+
+// 		gsap.timeline({
 // 			scrollTrigger: {
-// 				trigger: "portfolioDiv",
+// 				trigger: ".portfolio",
 // 				start: "top top",
 // 				end: "+=100%",
 // 				scrub: 1,
@@ -93,57 +98,46 @@ mediaQueryList.addEventListener('change', handlePortfolio);
 // 			}
 // 		})
 
-// 		.add("pic1")
-// 		.from(".slide--02", {yPercent: 100}, "pic1")
-// 		.to(".slideText--01", {opacity: 0}, "pic1")
-// 		.from(".slideText--02", {yPercent: -30, opacity: 0}, "pic1")
+// 			.add("pic1")
+// 			.from(".slide--02", {yPercent: 100}, "pic1")
+// 			.to(".slideText--01", {opacity: 0}, "pic1")
+// 			.from(".slideText--02", {yPercent: -30, opacity: 0}, "pic1")
 
-// 		.add("pic2")
-// 		.from(".slide--03", {yPercent: 100}, "pic2")
-// 		.to(".slideText--02", {opacity: 0}, "pic2")
-// 		.from(".slideText--03", {yPercent: -30, opacity: 0}, "pic2")
+// 			.add("pic2")
+// 			.from(".slide--03", {yPercent: 100}, "pic2")
+// 			.to(".slideText--02", {opacity: 0}, "pic2")
+// 			.from(".slideText--03", {yPercent: -30, opacity: 0}, "pic2")
 
-// 		.add("end")
+// 			.add("end")
+// 	},
 
-// 	} else {
-// 		gsap.from(".slideText--01", {
-// 		scrollTrigger: { 
-// 		trigger: ".slideText--01", 
-// 		start: "20% 100%",
-// 		end: "+=50%",
-// 		toggleActions: "play none none reset"
-// 		},
-// 		yPercent: 20, 
-// 		opacity: 0,
-// 		duration: 2,
+// 	// mobile
+// 	"(max-width: 899px)": function() {
+// 		// The ScrollTriggers created inside these functions are segregated and get
+// 		// reverted/killed when the media query doesn't match anymore. 
+
+// 		slideTexts.forEach(slideText => {
+// 			gsap.from(slideText, {
+// 				scrollTrigger: { 
+// 					trigger: slideText, 
+// 					start: "20% 100%",
+// 					end: "+=50%",
+// 					toggleActions: "play none none reset"
+// 				},
+// 				yPercent: 20, 
+// 				opacity: 0,
+// 				duration: 2,
+// 			});
 // 		});
-// 		gsap.from(".slideText--02", {
-// 		scrollTrigger: { 
-// 		trigger: ".slideText--02", 
-// 		start: "20% 100%",
-// 		end: "+=50%",
-// 		toggleActions: "play none none reset"
-// 		},
-// 		yPercent: 20, 
-// 		opacity: 0,
-// 		duration: 2,
-// 		});
-// 		gsap.from(".slideText--03", {
-// 		scrollTrigger: { 
-// 		trigger: ".slideText--03", 
-// 		start: "20% 100%",
-// 		end: "+=50%",
-// 		toggleActions: "play none none reset"
-// 		},
-// 		yPercent: 20, 
-// 		opacity: 0,
-// 		duration: 2,
-// 		});
+		
+// 	},
+
+// 	// all 
+// 	"all": function() {
+// 		// ScrollTriggers created here aren't associated with a particular media query,
+// 		// so they persist.
 // 	}
-// 	});
 // });
-
-// myObserver.observe(portfolioDiv);
 
 
 
