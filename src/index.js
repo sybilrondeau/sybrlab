b6fd35480c4d6ae028ebee80739ba6d6975763ad
sybil rondeau <sybilrondeau@macbook-pro-de-sybil-2.home>
2020-08-11 13:28:21 +0200

    responsivness

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-if (module.hot) {
-    module.hot.accept()
-}
-
 import './index.scss';
 import '../javascript/nav.js';
 import '../javascript/cursor.js';
-// import '../javascript/cta.js';
 import '../javascript/drawsvg.js';
-// import '../javascript/picturefill.js';
 import '../javascript/portfolio.js';
 import '../javascript/logo.js';
 
