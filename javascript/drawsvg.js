7d650a53132a3bfb5290fbd2862c56d9e0f8185f
Sybil Rondeau <contact@sybilrondeau.com>
2020-11-03 21:36:18 +0100

    set up draw svg on intro section

--- javascript/drawsvg.js
+++ javascript/drawsvg.js
@@ -3,22 +3,17 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin);
 
-gsap.from(".draw", {
+// draw portrait at page load
+gsap.from(".draw-portrait", { duration: 6, drawSVG: "50% 50%" });
+
+// draw camera when visible on page
+gsap.from(".draw-camera", {
 	scrollTrigger : {
-		trigger: ".draw",
+		trigger: ".camera-paragraph",
 		start: "top 80%",
 		toggleActions: "play none none reset",
+    markers: true
 	},
 	duration: 6,
 	drawSVG: "50% 50%"
-});
-
-gsap.from(".draw-portrait", {
-  scrollTrigger : {
-    trigger: ".a-propos__portrait",
-    start: "20% 80%",
-    toggleActions: "play none none reset",
-  },
-  duration: 6,
-  drawSVG: "50% 50%"
 });
